1b23e32aba1cff0e2bd63c40a77f05bad4f8a34d
João Vitor Nishimoto Bonfim <137560778+joaovitornishimoto@users.noreply.github.com>
2023-10-16 18:57:55 -0300

    atualizie lista de exercícos em javascript.

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,1 +1,6 @@
-console.log('O consola funcionou corretamente!')
+var vel = 78.2
+console.log(`A velocidade de seu carro é de ${vel}km/h`)
+if (vel > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
