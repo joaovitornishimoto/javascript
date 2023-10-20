72d64fdf723598b89e55b98c3512f26a6757fc65
João Vitor Nishimoto Bonfim <137560778+joaovitornishimoto@users.noreply.github.com>
2023-10-20 20:47:43 -0300

    atualizei.

--- aula012/teste001.js
+++ aula012/teste001.js
@@ -1,6 +1,22 @@
+console.log ('Bem vindo a calculadora digital')
 var n1 = 5
-if(n1 < 6) {
-    console.log(`O número digita é ${n1}.`)
-} else if (n1 <= 5){
-    console.log(`O número digitado é menor que 6 `)
+switch(n1) {
+    case 0:
+        var calc1 = n1 + 5
+        console.log(`Você escolheu a soma de 5 + 5 que é igual a ${calc1}`)
+        break
+    case 1:
+        var calc2 = n1 - 5
+        console.log(`Você escolheu subtrair 5 - 5 que é igual a ${calc2}`)
+        break
+    case 2:
+        var calc3 = n1 * 5
+        console.log(`Você escolheu multiplicar 5 * 5 que é igual a ${calc3}`)
+        break
+    case 3:
+        var calc4 = n1 / 5
+        console.log(`Você escolheu dividir 5 / 5 que é igual a ${calc4}`)
+        break
+    default:
+        break
 }
