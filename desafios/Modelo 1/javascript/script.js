c280ed5fe988df64239520ed34f75379d576fe70
João Vitor Nishimoto Bonfim <137560778+joaovitornishimoto@users.noreply.github.com>
2023-11-23 13:16:18 -0300

    atualizei o repositório.

--- desafios/Modelo 1/javascript/script.js
+++ desafios/Modelo 1/javascript/script.js
@@ -3,15 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    //var hora = 22
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        // BOM DIA!
+        //BOM DIA!
         img.src = 'manha-250.png'
+        document.body.style.background = '#bf9c68'
     } else if (hora >= 12 && hora < 18) {
-        // BOA TARDE!
+        //BOA TARDE!
         img.src = 'tarde-250.png'
+        document.body.style.background = '#97bc9b'
     } else {
         // BOA NOITE!
         img.src = 'noite-250.png'
+        document.body.style.background = '#0a181b'
     }
 }
