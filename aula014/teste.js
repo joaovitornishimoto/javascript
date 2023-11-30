259c3721d8f84031f124b63e6a10c932e858f72f
João Vitor Nishimoto Bonfim <137560778+joaovitornishimoto@users.noreply.github.com>
2023-11-30 14:13:01 -0300

    atualizei repositório.

---
+++ aula014/teste.js
@@ -0,0 +1,9 @@
+/*
+for (var nota = 1; nota <= 10; nota++) {
+    if(nota < 0 && nota <= 10) {
+        console.log("Uma boa nota.")
+    } else {
+        console.log("Valor inválido!")
+    }
+}
+*/
