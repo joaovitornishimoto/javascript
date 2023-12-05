5dc1ebce1ac4e84d3e9fdd1966f10922351e04d5
João Vitor Nishimoto Bonfim <137560778+joaovitornishimoto@users.noreply.github.com>
2023-12-05 11:48:40 -0300

    atualizei repositório.

--- ex002/javascript.js
+++ ex002/javascript.js
@@ -1,6 +1,33 @@
-function trocar() {
-    var num1 = Number(document.getElementById('txtn1').value)
-    var num2 = Number(document.getElementById('txtn2').value)
+function maior() {
+    var num1 = parseFloat(document.getElementById('txtn1').value)
+    var num2 = parseFloat(document.getElementById('txtn2').value)
+    var num3 = parseFloat(document.getElementById('txtn3').value)
 
-    
+    var maior = num1
+
+    if(num2 > maior) {
+        maior = num2
+    }
+    if(num3 > maior) {
+        maior = num3
+    }
+
+    alert("Maior: "+maior)
+}
+
+function menor() {
+    var num1 = parseFloat(document.getElementById('txtn1').value)
+    var num2 = parseFloat(document.getElementById('txtn2').value)
+    var num3 = parseFloat(document.getElementById('txtn3').value)
+
+    menor = num1
+
+    if(num2 < menor) {
+        menor = num2
+    }
+    if(num3 < menor) {
+        menor = num3
+    }
+
+    alert("Menor: "+menor)
 }
