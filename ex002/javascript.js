462a4dff59df2974a44bb78059c54743768453e0
João Vitor Nishimoto Bonfim <137560778+joaovitornishimoto@users.noreply.github.com>
2023-12-07 11:46:54 -0300

    update Javascript.

--- ex002/javascript.js
+++ ex002/javascript.js
@@ -1,27 +1,17 @@
-var valor_hora = prompt("Qual o valor da sua hora? ")
-valor_hora = parseInt(valor_hora)
-var qtde_trabalhada = prompt("quantas horas você trabalhou esse mês? ")
-qtde_trabalhada = parseInt(qtde_trabalhada)
-var salario_bruto = (valor_hora * qtde_trabalhada)
-alert("Seu salário bruto sem descontos é de " + salario_bruto)
+var nota1 = prompt("Digite sua nota no primeiro semestre: ")
+nota1 = parseInt(nota1)
+var nota2 = prompt("digite sua nota no segundo semestre: ")
+nota2 = parseInt(nota2)
+var media = nota1 + nota2 / 5
 
-if(salario_bruto > 0 && salario_bruto <= 900) {
-    salario_liq = salario_bruto - (salario_bruto * (10 / 100))
-    salario_liq = salario_bruto - (salario_bruto * (11 / 100))
-    alert("o seu salário atual é maior que R$ 0 e menor que R$ 900 " + salario_liq)
-} else if (salario_bruto > 900 && salario_bruto <= 1500) {
-    salario_liq = salario_bruto - (salario_bruto * (5 / 100))
-    salario_liq = salario_bruto - (salario_bruto * (10 / 100))
-    salario_liq = salario_bruto - (salario_bruto * (11 / 100))
-    alert("O seu salário atual é de " + salario_liq)
-} else if (salario_bruto > 1500 && salario_bruto <= 2500) {
-    salario_liq = salario_bruto - (salario_bruto * (10 / 100))
-    salario_liq = salario_bruto - (salario_bruto * (10 / 100))
-    salario_liq = salario_bruto - (salario_bruto * (11 / 100))
-    alert("O seu salário atual é maior que R$ 1500 e menor que R$ 2500 " + salario_liq)
-} else if (salario_bruto > 2500) {
-    salario_liq = salario_bruto - (salario_bruto * (20 / 100))
-    salario_liq = salario_bruto - (salario_bruto * (10 / 100))
-    salario_liq = salario_bruto - (salario_bruto * (11 / 100))
-    alert("O seu salário atual está acima de R$ 2500" + salario_liq)
+if (media >= 9 && media >= 10) {
+    alert("Sua nota foi A: APROVADO!" )
+} else if (media >= 7.5 && media <= 9) {
+    alert("Sua nota foi B: APROVADO!")
+} else if (media >= 6 && media <= 7.5) {
+    alert("Sua nota foi C: APROVADO!")
+} else if (media >= 4 && media <= 6) {
+    alert("Sua nota foi D: REPROVADO!")
+} else if (media <= 4 && media >= 0) {
+    alert("sua nota foi E: REPROVADO!")
 }
